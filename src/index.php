586ee00b603bfa389025eae5383d1d93ed2302ed
Carlos Gonzalez <siscond@hotmail.com>
2025-02-19 06:06:35 -0500

    my first commit

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP in Docker Test </title>
+</head>
+<body>
+    <ul>
+        <li><a href="database.php">Database</a></li>
+        <li><a href="hello.php">Hello world</a></li>
+    </ul>
+</body>
+</html>
